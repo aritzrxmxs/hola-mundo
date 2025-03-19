a0a1f46bb3b76afd7b72b49bb5054e38bf0a9ded
Aritz <aritz.ramos.lozares.al@iespoligonosur.org>
2025-03-19 09:29:28 +0100

    Add files via upload

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,9 @@
+/**
+Miestra por pantalla la frase "¡Hola Mundo!"
+
+@author Aritz Ramos
+*/
+
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola Mundo!");
