19f4e010aeba63356a86b23d68b94fbbd3053fc7
Aritz Ramos Lozares <aritz.ramos.lozares.al@iespoligonosur.org>
2025-03-27 12:53:02 +0100

    Saludo  ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,5 +7,6 @@ Miestra por pantalla la frase "¡Hola Mundo!"
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola Mundos!");
+System.out.println("¡Adios Mundo!");
 	}
 }
